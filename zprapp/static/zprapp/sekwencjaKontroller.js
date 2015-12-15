9cb654e512bbc6267c232f58f67f2945e0dbb479
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2015-12-16 00:53:21 +0100

    poprawione kopiowanie sekwencji, obsluga bledów + eliminacja niemożliwych prób kopiowania do schowka

--- zprapp/static/zprapp/sekwencjaKontroller.js
+++ zprapp/static/zprapp/sekwencjaKontroller.js
@@ -22,17 +22,40 @@ function SekwencjaKontroler($scope, $routeParams, $http, clipboard) {
     //$scope.scs = Items.sekwencja($routeParams.id_chr, $routeParams.id_sc);
     $scope.alert={}
     $scope.alert.show = true
+    $scope.alert.type="danger"
     $scope.alert.toggleShow = function(){
         $scope.alert.show = !$scope.alert.show
     }
-    //$scope.copySeq = function(){
-    //    clipboard.copyText($scope.scs)
-    //}
-    $scope.alert.msg = "ERROR"
-    $scope.ok = function(){
-        console.log("OK")
+
+    var limitCopy = 500000
+
+    $scope.copySeq = function(){
+        console.log($scope.scs.length)
+        //$scope.copyFail("copy error")
+        if($scope.scs.length > limitCopy){
+            $scope.copyFail("copy error (ponad " + limitCopy + " znaków)")
+        }
+        else{
+            try {
+                clipboard.copyText($scope.scs)
+                $scope.copySuccess()
+            } catch (err){
+                $scope.copyFail(err)
+            }
+
+            //console.log(a.scope.onCopied)
+            console.log("skopiowano")
+        }
     }
-    $scope.fail = function(err) {
+    //document.getElementById("copySeqBtn")
+    $scope.alert.msg = "ERROR"
+    $scope.copySuccess = function(){
+        console.log("skopiowano")
+        $scope.alert.msg = "Skopiowano sekwencję do schowka."
+        $scope.alert.type = "success"
+        $scope.alert.show=false
+    }
+    $scope.copyFail = function(err) {
         $scope.alert.msg = err + ":    za dużo znaków do skopiowania"
         $scope.alert.show=false
     }
