37fad59e869f0a0bca520374ec5da32b7da0692f
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2017-04-29 21:15:42 +0200

    dodana paginacja adnotacji w adminie

--- zprapp/admin.py
+++ zprapp/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from zprapp.models import Organism, Chromosome, AnnotationType, Annotation, Aggregation
 
 # Register your models here.
+class AnnotationAdmin(admin.ModelAdmin):
+    model = Annotation
+    list_per_page = 100
+
 admin.site.register(Organism)
 admin.site.register(Chromosome)
 admin.site.register(AnnotationType)
-admin.site.register(Annotation)
+admin.site.register(Annotation, AnnotationAdmin)
 admin.site.register(Aggregation)
