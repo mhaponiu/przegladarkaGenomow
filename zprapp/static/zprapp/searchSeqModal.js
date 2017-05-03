5f2213e7963034e6f4799b2e2fc4e0c7d10ac171
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2017-05-03 23:10:12 +0200

    frontend searchSeq z wyborem organizmu, chromosomu, typu

--- zprapp/static/zprapp/searchSeqModal.js
+++ zprapp/static/zprapp/searchSeqModal.js
@@ -1,8 +1,8 @@
 /**
  * Created by mhaponiu on 01.04.16.
  */
-angular.module('CucModule').controller('SearchSeqModalCtrl', function($scope, $modal){
-    $scope.open = function(){
+angular.module('CucModule').controller('SearchSeqModalCtrl', function ($scope, $modal) {
+    $scope.open = function () {
         var modalIntance = $modal.open({
             templateUrl: 'SearchSeqModalContent.html',
             controller: 'SearchSeqModalInstance',
@@ -10,7 +10,7 @@ angular.module('CucModule').controller('SearchSeqModalCtrl', function($scope, $m
             scope: $scope,
             size: "" //sm lg
         });
-        modalIntance.result.then(function(){
+        modalIntance.result.then(function () {
             console.log("CLOSE");
         });
     }
@@ -20,19 +20,19 @@ angular.module('CucModule').controller('SearchSeqModalCtrl', function($scope, $m
 })
 
 //controller wewnatrz okienka modalnego; ma przekazany scope od SearchSeqModalCtr
-angular.module('CucModule').controller('SearchSeqModalInstance', function($scope, $modalInstance, $http, $filter, DataBufor){
-    $scope.ok = function(){
+angular.module('CucModule').controller('SearchSeqModalInstance', function ($scope, $modalInstance, $http, $filter, DataBufor) {
+    $scope.ok = function () {
         console.log("OK");
         $modalInstance.dismiss();
     }
-    $scope.cancel = function(){
+    $scope.cancel = function () {
         $modalInstance.close();
     }
     $scope.algorytmy = [
-      {name:'KMP', id:1, params:{} },
-      {name:'Boyer', id:2, params:{} },
-      {name:'SW', id:3, params:{win:2, gap:4} },
-      {name:'BLAST', id:4, params:{w:7, t:3, c:3}},
+        {name: 'KMP', id: 1, params: {}},
+        {name: 'Boyer', id: 2, params: {}},
+        {name: 'SW', id: 3, params: {win: 2, gap: 4}},
+        {name: 'BLAST', id: 4, params: {w: 7, t: 3, c: 3}},
     ];
     $scope.wybranyAlg = $scope.algorytmy[0]
     $scope.wzorzec;
@@ -41,23 +41,72 @@ angular.module('CucModule').controller('SearchSeqModalInstance', function($scope
     $scope.wybranyOrg = $scope.orgs[0]
     $scope.showTable = false
 
+    // sciagniecie chromosomow dla organizmu
+    $scope.org_chromosomes = {}
+    loadChromosomes = function (org_id) {
+        var request = {
+            method: 'GET',
+            url: 'api/organisms/' + org_id + '/chromosomes/'
+        };
+        return $http(request)
+            .success(function (data) {
+                //trzeba odrazu dane wsadzac to obiektu scope - nie potrafie ich przekazac przez returna
+                $scope.org_chromosomes[org_id] = data;
+            });
+    }
+
+    //sciagniecie typow dla chromosomu
+    $scope.chr_types = {}
+    loadTypes = function (id_chr) {
+        var request = {
+            method: 'GET',
+            url: 'api/chromosomes/' + id_chr + '/annotation_types/'
+        };
+        return $http(request)
+            .success(function (data) {
+                //trzeba odrazu dane wsadzac to obiektu scope - nie potrafie ich przekazac przez returna
+                $scope.chr_types[id_chr] = data;
+            });
+    }
+
+    $scope.$watch('wybranyOrg', function () {
+        // $scope.wybranyChr_id= $scope.wybranyOrg.chromosomes[0]
+        // $scope.listaChr = $scope.wybranyOrg.chromosomes
+        loadChromosomes($scope.wybranyOrg.id).then(function () {
+            $scope.listChromosomow = $scope.org_chromosomes[$scope.wybranyOrg.id]
+            // debugger
+            $scope.wybranyChr = $scope.listChromosomow[0]
+            // console.log($scope.wybranyChr.number)
+        })
+    })
+    $scope.$watch('wybranyChr', function () {
+        loadTypes($scope.wybranyChr.id).then(function () {
+            $scope.listaTypow = $scope.chr_types[$scope.wybranyChr.id]
+            // console.log($scope.listaTypow)
+            $scope.wybranyTyp = $scope.listaTypow[0]
+            // console.log($scope.wybranyTyp.name)
+        })
+    })
+
 
     // TODO na sztywno wpisany zeby szybciej pokazac -> usunac nizej
     $scope.wzorzec = "CTTCAGCAAAG"
 
-    $scope.searchSeq = function(){
+    $scope.searchSeq = function () {
         var request = {
             method: 'POST',
             url: '/api/calc',
             //params: {param1: "p1p1p1", param2: "p2p2p2"}, //query string parametr
-            data: { pattern: $scope.wzorzec,
-                    alg_id: $scope.wybranyAlg['id'],
-                    alg_name: $scope.wybranyAlg['name'],
-                    alg_params: {dupa: 234},
-                    annotation_params: {
-                        chr_id: $scope.wybranyChr,
-                        org: $scope.wybranyOrg['id']
-                    }
+            data: {
+                pattern: $scope.wzorzec,
+                alg_id: $scope.wybranyAlg['id'],
+                alg_name: $scope.wybranyAlg['name'],
+                alg_params: {dupa: 234},
+                annotation_params: {
+                    chr_id: $scope.wybranyChr.id,
+                    org_id: $scope.wybranyOrg.id,
+                    type_id: $scope.wybranyTyp.id
+                }
             } //ukryte data w poscie
         };
         $http(request)
@@ -73,7 +122,7 @@ angular.module('CucModule').controller('SearchSeqModalInstance', function($scope
         DataBufor.setData('view_to', item['pos'] + $scope.wzorzec.length)
         DataBufor.setData('annotation_id', item['annotation_id'])
         // window.location.replace("#/organisms/" + item['org_id'] +"/chromosomes/" + item['chr_id'] + "/annotations")
-        window.location.replace("#/organisms/" + item['org_id'] +"/chromosomes/" + item['chr_id'] +'/types/'+ 54 + "/annotations")
+        window.location.replace("#/organisms/" + item['org_id'] + "/chromosomes/" + item['chr_id'] + '/types/' + 54 + "/annotations")
 
     }
 
