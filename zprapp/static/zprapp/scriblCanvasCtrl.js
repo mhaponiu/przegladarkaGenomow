6cab5e3ea7070d85d79c0eb0da1a66d05b62f69d
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2017-06-04 17:38:03 +0200

    pobieranie typów, pokolorowana tabelka

--- zprapp/static/zprapp/scriblCanvasCtrl.js
+++ zprapp/static/zprapp/scriblCanvasCtrl.js
@@ -6,19 +6,16 @@ function ScriblCanvasController($scope, $routeParams, $http) {
     $scope.id_chr = $routeParams.id_chr;
     $scope.id_org = $routeParams.id_org;
     $scope.test = "TEST"
+
     var canvas = document.getElementById('scribl')
     canvas.style.backgroundColor = "#f5f5f5";
     chart = new Scribl(canvas, 720);
     canvas.style.width = "900px"
-
     // <div id="scroll-wraper" style="width: 900px;">
-
     chart.laneSizes = 18;
     chart.laneBuffer = 2;
     chart.trackBuffer = 40;
     chart.glyph.text.color = 'white';
-
-
 
     track1 = chart.addTrack();
     gene1 = track1.addFeature( new BlockArrow('track1', 5, 750 , '-') );
@@ -35,28 +32,37 @@ function ScriblCanvasController($scope, $routeParams, $http) {
     gene6 = track2.addFeature( new BlockArrow('track2', 3500, 1500, '-') );
     chart.track2.name = 'track 2';
 
-    chart.scrollable = true;
-    chart.scrollValues = [200000, 250000];
-    chart.draw()
-    document.getElementById('scroll-wraper').style.width = "900px"
-    chart.draw()
+    $scope.types = []
+    $scope.loadTypes = function () {
+        var request = {
+            method: 'GET',
+            url: 'api/organisms/'+  $routeParams.id_org + '/chromosomes/' + $routeParams.id_chr + '/annotation_types/',
+        };
+        return $http(request)
+            .success(function (data) {
+                $scope.types = data;
+            });
+    }
+
+    $scope.loadTypes().then(function () {
+        console.log("########")
+    }).then(function () {
+        console.log($scope.types)
+    }).then(function () {
+        chart.scrollable = true;
+        chart.scrollValues = [200000, 250000];
+        chart.draw()
+        document.getElementById('scroll-wraper').style.width = "900px"
+        chart.redraw()
+    })
 
     $scope.button = function () {
-        // alert('zmniejszamy skale!')
-        chart.scale.min = 500
-        chart.scale.max = 1000
-        chart.tick.major.size = 1000;
-        chart.redraw()
-        // chart.draw()
+        alert('zmniejszamy skale!')
     }
-    // $scope.loadScaffolds = function(id_org, id_chr){
-    //     var request = {
-    //         method: 'GET',
-    //         url: 'api/organisms/'+$scope.id_org+'/chromosomes/'+$scope.chr_id+'/annotation_types/'+ $scope.id_type +'/annotations/'
-    //     };
-    //     return $http(request)
-    //         .success(function (data) {
-    //             $scope.scflds = data;
-    //         });
-    // }
+
+    // chart.scrollable = true;
+    // chart.scrollValues = [200000, 250000];
+    // chart.draw()
+    // document.getElementById('scroll-wraper').style.width = "900px"
+    // chart.redraw()
 }
