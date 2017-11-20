72d2802610792425eea0918307ebcedebc0183c3
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2017-11-20 01:12:44 +0100

    singletony class, singletony method w CombinationData

--- zpr/database/v4/data_structures.py
+++ zpr/database/v4/data_structures.py
@@ -21,10 +21,16 @@ def singleton(func):
 
 
 class FastaData():
+    def __call__(self):
+        # singleton w polaczeniu z wywolaniem po definicji klasy
+        return self
+
     def __init__(self):
+        print('wczytywanie fasta...')
         generator = parser.Fasta_454().generator()
         self.record_list = list(generator) #fasta records from file
         ''' FastaRecord, ["id_from_file", "id", "sequence"] '''
+        print('...koniec wczytywania fasta')
 
     def __len__(self):
         return len(self.records)
@@ -46,14 +52,20 @@ class FastaData():
     @singleton
     def sum_all_sequences(self):
         return sum([len(seq) for seq in self.records])
+FastaData = FastaData()
 
 
 class GffData():
+    def __call__(self):
+        # singleton w polaczeniu z wywolaniem po definicji klasy
+        return self
 
     def __init__(self):
+        print('wczytywanie gff...')
         generator = parser.Gff_annotation_abinitio_ill1().generator()
         self.record_list = list(generator)  # gff records from file
         ''' GffRecord, ['ctg_id','type', 'start', 'end', 'info'] '''
+        print('...koniec wczytywania gff')
 
     def __len__(self):
         return len(self.records)
@@ -84,33 +96,42 @@ class GffData():
     def types_amount(self):
         # todo
         pass
+GffData = GffData()
 
 
 
 class CombinationData():
     ''' wspolne contigi miedzy fasta i gff to wszystkie te co w gffach sa '''
+
+    def __call__(self):
+        # singleton w polaczeniu z wywolaniem po definicji klasy
+        return self
+
     def __init__(self):
         self.fasta_data = FastaData()
         self.gff_data = GffData()
 
-
+    @property
+    @singleton
     def joint_ctg_id_list(self):
         ''' tyle co gffow zgodnie z przewidywaniami'''
         return sorted(list(set(self.gff_data.contigs_id_list).intersection(self.fasta_data.contigs_id_list)))
 
     def joint_ctgs_amount(self):
         ''' tyle co gffow zgodnie z przewidywaniami'''
-        return len(self.joint_ctg_id_list())
+        return len(self.joint_ctg_id_list)
 
+    @property
+    @singleton
     def dict_fasta_filtered_by_gff_contigs(self):
         ''' slownik jak w fasta_data.dict ale tylko dla tych co w gffach sa'''
         fasta_dict = self.fasta_data.dict
         gff_contigs_ids = self.gff_data.contigs_id_list
-        d = { key:value for (key, value) in fasta_dict.items() if key in gff_contigs_ids}
-        return d
+        return { key:value for (key, value) in fasta_dict.items() if key in gff_contigs_ids}
 
-
-
+    @singleton
     def sum_sequences(self):
         ''' joint sequences gff and fasta'''
-        di = { key:value for (key, value) in self.fasta_data.dict.items() if key in self.gff_data.contigs_id_list()}
+        return sum([len(seq) for seq in self.dict_fasta_filtered_by_gff_contigs.values()])
+
+CombinationData = CombinationData()
